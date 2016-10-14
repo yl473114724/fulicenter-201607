2e0f6251dba57a706a793718d5fcc60b708c043b
Yanglei <473114724@qq.com>
2016-10-14 17:43:48 +0800

    添加log输出的工具类，并在MainActivity中测试

--- xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/Activity/MainActivity.java
+++ xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/Activity/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import cn.ucai.xm_fulicenter.R;
+import cn.ucai.xm_fulicenter.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i("MainActivity onCreate");
+
     }
 }
