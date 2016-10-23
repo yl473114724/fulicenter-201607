757b2e72dfa70bc788acfc49983756c258b45897
Yanglei <473114724@qq.com>
2016-10-23 18:52:27 +0800

    添加注册页面，并绑定登录页面的跳转

---
+++ xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/Activity/RegisterActivity.java
@@ -0,0 +1,29 @@
+package cn.ucai.xm_fulicenter.Activity;
+
+import android.os.Bundle;
+
+import cn.ucai.xm_fulicenter.R;
+
+public class RegisterActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    protected void setListener() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
--- xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/utils/MFGT.java
+++ xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/utils/MFGT.java
@@ -11,6 +11,7 @@ import cn.ucai.xm_fulicenter.Activity.CategoryChildActivity;
 import cn.ucai.xm_fulicenter.Activity.GoodsDatailActivity;
 import cn.ucai.xm_fulicenter.Activity.LoginActivity;
 import cn.ucai.xm_fulicenter.Activity.MainActivity;
+import cn.ucai.xm_fulicenter.Activity.RegisterActivity;
 import cn.ucai.xm_fulicenter.I;
 import cn.ucai.xm_fulicenter.R;
 import cn.ucai.xm_fulicenter.bean.BoutiqueBean;
@@ -32,37 +33,42 @@ public class MFGT {
     public static void startActivity(Activity context, Class<?> cls) {
         Intent intent = new Intent();
         intent.setClass(context, cls);
-        startActivity(context,intent);
+        startActivity(context, intent);
     }
 
     public static void gotoGoodsDatailActivity(Context context, int goodsId) {
         Intent intent = new Intent();
         intent.setClass(context, GoodsDatailActivity.class);
         intent.putExtra(I.GoodsDetails.KEY_GOODS_ID, goodsId);
-        startActivity(context,intent);
+        startActivity(context, intent);
     }
 
-    public static void startActivity(Context context,Intent intent) {
+    public static void startActivity(Context context, Intent intent) {
         context.startActivity(intent);
-        ((Activity)context).overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
+        ((Activity) context).overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
     }
+
     public static void gotoBoutiqueChildActivity(Context context, BoutiqueBean bean) {
         Intent intent = new Intent();
         intent.setClass(context, BoutiqueChildActivity.class);
         intent.putExtra(I.Boutique.CAT_ID, bean);
-        startActivity(context,intent);
+        startActivity(context, intent);
     }
+
     public static void gotoCategoryChildActivity(Context context, int catId, String groupName, ArrayList<CategoryChildBean> list) {
         Intent intent = new Intent();
         intent.setClass(context, CategoryChildActivity.class);
         intent.putExtra(I.CategoryChild.CAT_ID, catId);
         intent.putExtra(I.CategoryGroup.NAME, groupName);
         intent.putExtra(I.CategoryChild.ID, list);
-        startActivity(context,intent);
+        startActivity(context, intent);
     }
 
     public static void gotoLogin(Activity context) {
         startActivity(context, LoginActivity.class);
+    }
 
+    public static void gotoRegister(Activity context) {
+        startActivity(context, RegisterActivity.class);
     }
 }
