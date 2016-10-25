d67fc80bde4516627396c35d59e9bd24e2f1cdc6
Yanglei <473114724@qq.com>
2016-10-26 03:14:38 +0800

    更新用户昵称以及更新用户头像

--- xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/Activity/UserProfileActivity.java
+++ xm-fulicenter/src/main/java/cn/ucai/xm_fulicenter/Activity/UserProfileActivity.java
@@ -1,19 +1,31 @@
 package cn.ucai.xm_fulicenter.Activity;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.File;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import cn.ucai.xm_fulicenter.FuLiCenterApplication;
+import cn.ucai.xm_fulicenter.I;
 import cn.ucai.xm_fulicenter.R;
+import cn.ucai.xm_fulicenter.bean.Result;
 import cn.ucai.xm_fulicenter.bean.User;
 import cn.ucai.xm_fulicenter.dao.SharePrefrenceUtils;
+import cn.ucai.xm_fulicenter.net.NetDao;
+import cn.ucai.xm_fulicenter.net.OkHttpUtils;
+import cn.ucai.xm_fulicenter.utils.CommonUtils;
 import cn.ucai.xm_fulicenter.utils.ImageLoader;
+import cn.ucai.xm_fulicenter.utils.L;
 import cn.ucai.xm_fulicenter.utils.MFGT;
+import cn.ucai.xm_fulicenter.utils.OnSetAvatarListener;
+import cn.ucai.xm_fulicenter.utils.ResultUtils;
 import cn.ucai.xm_fulicenter.view.DisplayUtils;
 
 
@@ -28,6 +40,7 @@ public class UserProfileActivity extends BaseActivity {
 
     UserProfileActivity mContext;
     User user = null;
+    OnSetAvatarListener mOnSetAvatarListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,18 +52,17 @@ public class UserProfileActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        DisplayUtils.initBackWithTitle(mContext,getResources().getString(R.string.user_profile));    }
+        DisplayUtils.initBackWithTitle(mContext,getResources().getString(R.string.user_profile));
+    }
 
     @Override
     protected void initData() {
         user = FuLiCenterApplication.getUser();
-        if (user != null) {
-            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,mIvUserProfileAvatar);
-            mTvUserProfileName.setText(user.getMuserName());
-            mTvUserProfileNick.setText(user.getMuserNick());
-        } else {
+        if (user == null) {
             finish();
+            return;
         }
+        showInfo();
     }
 
     @Override
@@ -62,15 +74,80 @@ public class UserProfileActivity extends BaseActivity {
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.layout_user_profile_avatar:
+                mOnSetAvatarListener = new OnSetAvatarListener(mContext,R.id.layout_upload_avatar,
+                        user.getMuserName(), I.AVATAR_TYPE_USER_PATH);
                 break;
             case R.id.layout_user_profile_username:
+                CommonUtils.showLongToast(R.string.username_connot_be_modify);
                 break;
             case R.id.layout_user_profile_nickname:
+                MFGT.gotoupdateNick(mContext);
                 break;
             case R.id.btn_logout:
                 logout();
                 break;
         }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        showInfo();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        L.e("onActivityResult,requestCode="+requestCode+",resultCode="+resultCode);
+        if(resultCode!=RESULT_OK){
+            return;
+        }
+        mOnSetAvatarListener.setAvatar(requestCode,data,mIvUserProfileAvatar);
+        if(requestCode== I.REQUEST_CODE_NICK){
+            CommonUtils.showLongToast(R.string.update_user_nick_success);
+        }
+        if(requestCode==OnSetAvatarListener.REQUEST_CROP_PHOTO){
+            updateAvatar();
+        }
+    }
+
+    private void updateAvatar() {
+        File file = new File(OnSetAvatarListener.getAvatarPath(mContext,
+                user.getMavatarPath()+"/"+user.getMuserName()
+                        +I.AVATAR_SUFFIX_JPG));
+        L.e("file="+file.exists());
+        L.e("file="+file.getAbsolutePath());
+        final ProgressDialog pd = new ProgressDialog(mContext);
+        pd.setMessage(getResources().getString(R.string.update_user_avatar));
+        pd.show();
+        NetDao.updateAvatar(mContext, user.getMuserName(), file, new OkHttpUtils.OnCompleteListener<String>() {
+            @Override
+            public void onSuccess(String s) {
+                L.e("s="+s);
+                Result result = ResultUtils.getResultFromJson(s,User.class);
+                L.e("result="+result);
+                if(result==null){
+                    CommonUtils.showLongToast(R.string.update_user_avatar_fail);
+                }else{
+                    User u = (User) result.getRetData();
+                    if(result.isRetMsg()){
+                        FuLiCenterApplication.setUser(u);
+                        ImageLoader.setAvatar(ImageLoader.getAvatarUrl(u),mContext,mIvUserProfileAvatar);
+                        CommonUtils.showLongToast(R.string.update_user_avatar_success);
+                    }else{
+                        CommonUtils.showLongToast(R.string.update_user_avatar_fail);
+                    }
+                }
+                pd.dismiss();
+            }
+
+            @Override
+            public void onError(String error) {
+                pd.dismiss();
+                CommonUtils.showLongToast(R.string.update_user_avatar_fail);
+                L.e("error="+error);
+            }
+        });
     }
 
     private void logout() {
@@ -80,5 +157,14 @@ public class UserProfileActivity extends BaseActivity {
             MFGT.gotoLogin(mContext);
         }
         finish();
+    }
+
+    private void showInfo() {
+        user = FuLiCenterApplication.getUser();
+        if (user != null) {
+            ImageLoader.setAvatar(ImageLoader.getAvatarUrl(user),mContext,mIvUserProfileAvatar);
+            mTvUserProfileName.setText(user.getMuserName());
+            mTvUserProfileNick.setText(user.getMuserNick());
+        }
     }
 }
